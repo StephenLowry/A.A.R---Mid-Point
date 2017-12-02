02dc62b47f66043732caa80da80c58ecbde326a0
StephenLowry <34129446+StephenLowry@users.noreply.github.com>
2017-12-02 21:33:49 +0000

    Initial commit

---
+++ Assets/Code/FireGun.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireGun : MonoBehaviour {
+
+    Animator anim;
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+	}
+
+    private void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            anim.SetTrigger("Shoot");
+        }
+    }
+}
